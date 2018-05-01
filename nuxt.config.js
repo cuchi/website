dba1ea146b7233a1ec8df4ffd4fae4a63fd7515d
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2018-05-01 01:04:39 -0300

    Viewport & meta description

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,9 +1,14 @@
 
 module.exports = {
-    head: {
-        titleTemplate: '%s - cuchi.me'
-    },
-    css: [
-        '@/assets/css/spectre-custom.scss'
+  head: {
+    titleTemplate: '%s - cuchi.me',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Hi there! this page is where I share my thoughs and knowledge about software development.' }
     ]
+  },
+  css: [
+    '@/assets/css/spectre-custom.scss'
+  ]
 }
