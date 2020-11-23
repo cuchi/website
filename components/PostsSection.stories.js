14da35cd2aa3224f90f92170977ba50454fd4811
Paulo Henrique Cuchi <paulo.cuchi@gmail.com>
2020-11-23 01:02:30 -0300

    chore: use control args on storybook

--- components/PostsSection.stories.js
+++ components/PostsSection.stories.js
@@ -1,18 +1,21 @@
-import PostsSection from './PostsSection'
+import PostsSectionComponent from './PostsSection'
 
-export default { title: "Posts Section" }
-
-const Template = () => ({
-    components: { PostsSection },
-    props: ["posts"],
-    template: '<PostsSection :posts="posts"/>',
-})
-
-export const Simple = Template.bind({})
-Simple.args = {
-    posts: [{
-        name: "this-is-an-article-name",
-        title: "Some cool article",
-        createdAtPretty: "Some time ago",
-    }]
+export default {
+    title: "Posts Section",
+    component: PostsSectionComponent,
+    argTypes: {
+        posts: {
+            defaultValue: [{
+                name: "this-is-an-article-name",
+                title: "Some cool article",
+                createdAtPretty: "Some time ago",
+            }]
+        }
+    }
 }
+
+export const PostsSection = (arg, { argTypes }) => ({
+    components: { PostsSectionComponent },
+    props: Object.keys(argTypes),
+    template: '<PostsSectionComponent :posts="posts" />'
+})
